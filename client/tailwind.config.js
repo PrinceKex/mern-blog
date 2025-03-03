b2d0605c75c46e612c0cf3b44f76132df26c4e88
PrinceKex <ehigiator.ukeke@gmail.com>
2025-03-03 15:30:25 +0100

    createRequire added for tailwindconfig

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,14 @@
+import { createRequire } from 'module'
+const require = createRequire(import.meta.url)
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
-    'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require('flowbite/plugin'),
-    require('tailwind-scrollbar'),
-  ],
-};
+ content: [
+  './index.html',
+  './src/**/*.{js,ts,jsx,tsx}',
+  'node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}',
+ ],
+ theme: {
+  extend: {},
+ },
+ plugins: [require('flowbite/plugin'), require('tailwind-scrollbar')],
+}
